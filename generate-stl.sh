501aa2d69248c6aea8b570b4744a699b1cc01998
Vadym Vikulin <vadym.vikulin@rivchain.org>
2025-11-07 21:31:11 +0200

    Assembly guide

--- generate-stl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p build
-for file in ./Enclosure/*.scad; do
-    filePath="${file%.*}"
-    inferredPath=$(echo "$filePath" | sed "s/.\/Enclosure\///g")
-    # For debugging
-    # echo "$filePath -> $inferredPath"
-    openscad "$filePath.scad" -o "./build/$inferredPath.stl"
-done
